0a8cf6b787ff4be737f0dbe954ac5a3e29164dbf
Rahul <newrahulgautam@gmail.com>
2024-12-22 16:44:38 +0530

    Update24.12.22.01

--- Spring-Framework-Weekend/servlet-simple-crud-operation (1).zip_expanded/servlet-simple-crud-operation/src/main/java/com/ibm/servlet_simple_crud_operation/controller/InsertStudentController.java
+++ Spring-Framework-Weekend/servlet-simple-crud-operation (1).zip_expanded/servlet-simple-crud-operation/src/main/java/com/ibm/servlet_simple_crud_operation/controller/InsertStudentController.java
@@ -15,12 +15,18 @@ import jakarta.servlet.http.HttpServletResponse;
 
 public class InsertStudentController extends HttpServlet{
 
-	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8577295209174342351L;
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		//Used to create object of StudentDao class
 		StudentDao dao = new StudentDao();
 		
+		//object of printWriter method will be created
 		PrintWriter p=resp.getWriter();
 		
 		String firstName=req.getParameter("firstName");
@@ -41,14 +47,17 @@ public class InsertStudentController extends HttpServlet{
 		
 		String address=req.getParameter("address");
 		
+		//Student data will be passed in student object
 		Student student = new Student(firstName, lastName, nickName, email, password, LocalDate.parse(dob), mobile, gender, address);
 		
-		
+		//Student data will be saved calling dao object of Student dao class calling saveStudentDao method passing student
 		Student student2=dao.saveStudentDao(student);
 		
 		if(student2!=null) {
+			//Data stored Message will be shown in page
 			p.write("<html><body><h1 style='color:green;'>Data Stored</h1></body></html>");
 		
+			//Register page will be displayed
 			RequestDispatcher dispatcher=req.getRequestDispatcher("register.jsp");
 			dispatcher.include(req, resp);
 		}
