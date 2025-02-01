76dfec32a862866b94493113b71e7f9de544e5b1
Rahul <newrahulgautam@gmail.com>
2025-02-01 18:54:20 +0530

    Update25.02.01.01

--- Spring-Framework-Weekend/ecommerce_jee_projects/src/main/java/ecommerce_jee_projects/controller/UnverifyProductOwnerController.java
+++ Spring-Framework-Weekend/ecommerce_jee_projects/src/main/java/ecommerce_jee_projects/controller/UnverifyProductOwnerController.java
@@ -1,13 +1,25 @@
 package ecommerce_jee_projects.controller;
 
-public class UnverifyProductOwnerController {
+import java.io.IOException;
 
-	/*
-	protected doPost() {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(value="/unverifyUser")
+public class UnverifyProductOwnerController extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req,HttpServletResponse resp) throws ServletException,IOException  {
 		
+		int id = Integer.parseInt(req.getParameter("id"));
 		
+		ProductOwnerDao dao = new ProductOwnerDaoImpl();
 	}
-	*/
+	
+	
 	
 	
 }
